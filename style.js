6e98602f8a216b0fcd052601dde493bec41e3c1c
OmriAlster <75723753+OmriAlster@users.noreply.github.com>
2021-07-03 23:09:18 +0300

    Add files via upload

---
+++ style.js
@@ -0,0 +1,13 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+    screen: {
+      padding: 50,
+      alignItems: 'center'
+    },
+    goalsList: {
+      
+    }
+  });
+
+export default styles;
